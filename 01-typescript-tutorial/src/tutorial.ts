80f8ec7c0840bbdffe724f5a13a7ab2a38ff459e
marrcelp <marcel.piaszczyk@gmail.com>
2024-08-01 18:29:33 +0200

    resolved challange no 12 - instanceof

--- 01-typescript-tutorial/src/tutorial.ts
+++ 01-typescript-tutorial/src/tutorial.ts
@@ -426,14 +426,66 @@
 // - Now you can call the printLength function with a string, null, or undefined as the argument. The function will print the length of the string if a string is provided, or 'No string provided' otherwise.
 
 
-function printLength(str : string | null | undefined) {
-    if (typeof str === 'string') {
-        console.log(str.length)
-    } else {
-        console.log('No string provided')
+// function printLength(str : string | null | undefined) {
+//     if (typeof str === 'string') {
+//         console.log(str.length)
+//     } else {
+//         console.log('No string provided')
+//     }
+// }
+//
+// printLength('dupa');
+// printLength(null);
+// printLength(undefined);
+
+
+// ## Challenge 12 - "instanceof" type guard
+//
+// The instanceof type guard is a way in TypeScript to check the specific class or constructor function of an object at runtime. It returns true if the object is an instance of the class or created by the constructor function, and false otherwise.
+//
+//     ```ts
+// try {
+//   // Some code that may throw an error
+//   throw new Error('This is an error');
+// } catch (error) {
+//   if (error instanceof Error) {
+//     console.log('Caught an Error object: ' + error.message);
+//   } else {
+//     console.log('Caught an unknown error');
+//   }
+// }
+// ```
+//
+// - Start by defining the function using the function keyword followed by the function name, in this case checkInput.
+// - Define the function's parameter. The function takes one parameter, input, which can be of type Date or string. This is denoted by input: Date | string.
+// - Inside the function, use an if statement to check if the input is an instance of Date. This is done using the instanceof operator.
+// - If the input is an instance of Date, return the year part of the date as a string. This is done by calling the getFullYear method on the input and then converting it to a string using the toString method.
+// - If the input is not an instance of Date (which means it must be a string), return the input as it is.
+// - After defining the function, you can use it by calling it with either a Date or a string as the argument. The function will return the year part of the date if a Date is passed, or the original string if a string is passed.
+// - You can store the return value of the function in a variable and then log it to the console to see the result.
+
+try {
+  // Some code that may throw an error
+  throw new Error('This is an error');
+} catch (error) {
+  if (error instanceof Error) {
+    console.log('Caught an Error object: ' + error.message);
+  } else {
+    console.log('Caught an unknown error');
+  }
+}
+
+function checkInput(input : Date | string){
+    if (input instanceof Date) {
+        return input.getFullYear().toString();
+    }
+    else {
+        return input;
     }
 }
 
-printLength('dupa');
-printLength(null);
-printLength(undefined);
+const data = checkInput(new Date());
+const random = checkInput('01-08-2024');
+
+console.log(data);
+console.log(random);
